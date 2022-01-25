b8213f03dcfb7dc43f7b46c99fa83fd69d175e70
kokikudo <massanmesu@icloud.com>
2022-01-25 16:18:00 +0900

    音声再生処理とタップ時のボタン背景透過値を変更する処理を実装

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,36 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        if let buttonText = sender.titleLabel!.text {
+            playSound(soundName: buttonText)
+            
+            sender.alpha = 0.5
+            
+            /*
+             Dispatch: システムが管理しているDispatchQueueに作業を送信することで、複数の処理を同時に行うためのフレームワーク。
+             メインスレッドにキューを作成し、コードブロック内の処理を指定の時間（0.2秒後）に実行する非同期処理を実装
+             */
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                sender.alpha = 1.0
+            }
+        }
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
+    
 }
 
